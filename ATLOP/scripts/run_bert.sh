d7dd5eb6ae6531bd3a67511e8763af28a2d30b55
Choi <schoi@uos.ac.kr>
2024-11-30 17:31:10 +0900

    원본 성능을 재현한 ATLOP 모델의 코드 스냅샵 저장
    seed 값을 동일한 값(66)으로 설정하였음에도 F1 스코어가 다르게 측정됨[60.78, 61.21].

---
+++ ATLOP/scripts/run_bert.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --nodes=1 
+#SBATCH --partition=edu1
+#SBATCH --cpus-per-task=48
+#SBATCH --gres=gpu:4
+#SBATCH --job-name=DRE
+#SBATCH -o /home2/csh102/DocRE/slurm/out/%j_%N.out  # STDOUT 
+#SBATCH -e /home2/csh102/DocRE/slurm/err/%j_%N.err  # STDERR
+
+echo "start at:" `date` 
+echo "node: $HOSTNAME" 
+echo "jobid: $SLURM_JOB_ID" 
+echo "--------------------------------------"
+
+module unload CUDA/11.2.2 
+module load cuda/11.3.1
+
+/home2/csh102/anaconda3/envs/240504-gnn-survey/bin/python /home2/csh102/DocRE/ATLOP/code/train.py \
+    --data_dir /home2/csh102/DocRE/docred \
+    --train_file train_annotated.json \
+    --dev_file dev.json \
+    --test_file dev.json \
+    \
+    --transformer_type bert \
+    --model_name_or_path bert-base-cased \
+    --train_batch_size 4 \
+    --test_batch_size 8 \
+    --gradient_accumulation_steps 1 \
+    --num_labels 4 \
+    --learning_rate 5e-5 \
+    --max_grad_norm 1.0 \
+    --warmup_ratio 0.06 \
+    --num_train_epochs 30.0 \
+    --seed 66 \
+    --num_class 97
